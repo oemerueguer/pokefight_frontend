1cae08ba9ae4f5e58ff17316185f67621b92791d
Lodos <o.ueguer@gmail.com>
2021-05-26 00:17:44 +0200

    gameboy styling done

--- src/components/Instruction/Instruction.js
+++ src/components/Instruction/Instruction.js
@@ -1,4 +1,5 @@
 import "../Instruction/Instruction.css";
+import { Link } from 'react-router-dom';
 
 export default function Instruction() {
   return (
@@ -6,12 +7,13 @@ export default function Instruction() {
       <div className="gameboy">
         <div className="gameboyScreen">
           <h2>Welcome to our Pokemon World </h2>
-          <h3>This is the instruction to the Game: </h3>
+          <h3>Read and then start the game </h3>
           <p>
             First of all, you need to choose a Pokemon by selecting on the
             desired pokemon and clicking SELECT. Afterwards, I have to choose
             another one with whom you want to fight with. Finally, you can click
             on FIGHT button.
+            <br />
             <br />
             To start, click on A Button.
           </p>
@@ -32,7 +34,7 @@ export default function Instruction() {
           </button>
         </div>
         <div className="PressBoard">
-          <button className="aPress">A</button>
+         <Link to="/home"><button className="aPress">A</button></Link>
           <button className="bPress">B</button>
         </div>
       </div>
