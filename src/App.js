53f656b280504bf073d646f67d9b402d2d38bbff
Lodos <o.ueguer@gmail.com>
2021-05-26 01:17:21 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch, Route } from 'react-router';
 import HomePage from './components/HomePage/HomePage';
 import Instruction from './components/Instruction/Instruction'
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route exact path= "/"><Instruction /></Route>
         <Route exact path= "/home"><HomePage /></Route>
         <Route exact path= "/pokemon/:id"></Route>
+        <Route exact path= "/footer"><Footer /></Route>
       </Switch>
     </div>
   );
