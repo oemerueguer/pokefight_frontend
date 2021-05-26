af1a51db64c73aaacabb173ec730ac0a67c6c306
Lodos <o.ueguer@gmail.com>
2021-05-27 00:31:46 +0200

    About site is ready

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,102 @@
-import React from 'react';
+import React from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBButton,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCol,
+} from "mdb-react-ui-kit";
 
-
-function About() {
+const About = () => {
   return (
-    <div className="about">
-        <h1>About</h1>
-    </div>
+    <MDBContainer className="p-4 ">
+      <MDBRow>
+        <MDBCol lg='3' md='12' className='mb-4 mb-md-0 mt-2' style={{ maxWidth: "22rem" }}>
+          <MDBCard>
+            <MDBCardImage
+              className="img-fluid"
+              src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+              waves
+            />
+            <MDBCardBody>
+              <MDBCardTitle>Jasmin Mingers</MDBCardTitle>
+              <MDBCardText>
+               Full - Stack Developer
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+
+        <MDBCol lg='3' md='12' className='mb-4 mb-md-0 mt-2' style={{ maxWidth: "22rem" }}>
+          <MDBCard>
+            <MDBCardImage
+              className="img-fluid"
+              src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+              waves
+            />
+            <MDBCardBody>
+              <MDBCardTitle>Clemens Dinkel</MDBCardTitle>
+              <MDBCardText>
+               Full - Stack Developer
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+
+        <MDBCol lg='3' md='12' className='mb-4 mb-md-0 mt-2' style={{ maxWidth: "22rem" }}>
+          <MDBCard>
+            <MDBCardImage
+              className="img-fluid"
+              src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+              waves
+            />
+            <MDBCardBody>
+              <MDBCardTitle>Stavros Temertzidis</MDBCardTitle>
+              <MDBCardText>
+               Full - Stack Developer
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+
+        <MDBCol lg='3' md='12' className='mb-4 mb-md-0 mt-2' style={{ maxWidth: "22rem" }}>
+          <MDBCard>
+            <MDBCardImage
+              className="img-fluid"
+              src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+              waves
+            />
+            <MDBCardBody>
+              <MDBCardTitle>Said R. Allem</MDBCardTitle>
+              <MDBCardText>
+               Full - Stack Developer
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+
+        <MDBCol lg='3' md='12' className='mb-4 mb-md-0 mt-2' style={{ maxWidth: "22rem" }}>
+          <MDBCard>
+            <MDBCardImage
+              className="img-fluid"
+              src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+              waves
+            />
+            <MDBCardBody>
+              <MDBCardTitle>Ömer Ügür</MDBCardTitle>
+              <MDBCardText>
+               Full - Stack Developer
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
-}
+};
 
 export default About;
