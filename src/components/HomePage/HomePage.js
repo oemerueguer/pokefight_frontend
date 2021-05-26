8dafabdb35436802d0fc390e9c288027d717c557
Ballerheinz <cle.dinkel@gmail.com>
2021-05-26 10:16:18 +0200

    initial fetch working, display of types and searchbar for filtering

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,14 @@
-export default function HomePage() {
-    return (
-      <div className="wrapper">
-        <p>Home</p>
-      </div>
-    );
-  }
+import HomeTypeIcons from './HomeTypeIcons';
+import PokeList from './PokeList';
+import SearchBar from './SearchBar';
+import './HomePage.css';
+
+export default function HomePage({data, query, setQuery}) {
+  return (
+    <div className="home">
+      <SearchBar query={query} setQuery={setQuery}/>
+      <HomeTypeIcons />
+      <PokeList data={data}/>
+    </div>
+  );
+}
---
+++ src/components/Pokemon/Pokemon.js
@@ -0,0 +1,7 @@
+export default function Pokemon() {
+    return (
+      <div className="wrapper">
+        <p>Pikaaaaa</p>
+      </div>
+    );
+  }
