7142ef3cd3aa5e18e0bdfbc42a066254fd9c5232
Ballerheinz <cle.dinkel@gmail.com>
2021-06-03 19:09:35 +0200

    change pagination

--- src/components/HomePage/HandPagination.js
+++ src/components/HomePage/HandPagination.js
@@ -1,6 +1,6 @@
 const HandPagination = ({ pokemonsPerPage, totalPokemons, paginate }) => {
     const pageNumbers = [];
-    const ceil = Math.ceil(totalPokemons / pokemonsPerPage) 
+    const ceil = Math.ceil(totalPokemons / pokemonsPerPage)
     const pageCount = ceil >= 10 ? 10 : ceil;
     for (let i = 1; i <= pageCount; i++) {
         pageNumbers.push(i);
@@ -8,17 +8,19 @@ const HandPagination = ({ pokemonsPerPage, totalPokemons, paginate }) => {
     console.log(ceil)
 
     return (
-        <nav className="page-wrapper">
-            <ul className="pagination">
-                {pageNumbers.map(number => (
-                    <li key={number} className='page-item'>
-                        <p onClick={() => paginate(number)} className='page-link'>
-                            {number}
-                        </p>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+        <>
+            <nav className="page-wrapper">
+                <ul className="pagination">
+                    {pageNumbers.map(number => (
+                        <li key={number} className='page-item'>
+                            <p onClick={() => paginate(number)} className='page-link'>
+                                {number}
+                            </p>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </>
     );
 };
 
